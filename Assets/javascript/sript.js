87160235f5c51ca50ad2ebbdea79c37925d9ab38
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-02-28 21:32:37 -0600

    Handle on buttons

--- Assets/javascript/sript.js
+++ Assets/javascript/sript.js
@@ -8,22 +8,31 @@ var today = moment().format('MMMM Do YYYY');
  console.log(today);
  $('#currentDay').text(today);
 
-var form9 = $('').val();
-var form10 = $('#10').val();
-var form11 = $('#11').val();
-var form12 = $('#12').val();
-var form1 = $('#1').val();
-var form2 = $('#2').val();
-var form3 = $('#3').val();
-var form4 = $('#4').val();
-var form5 = $('#5').val();
+// Getting a handle on the forms 
+var form9 = $('#9');
+var form10 = $('#10');
+var form11 = $('#11');
+var form12 = $('#12');
+var form1 = $('#1');
+var form2 = $('#2');
+var form3 = $('#3');
+var form4 = $('#4');
+var form5 = $('#5');
 
-var saveBtn = $('.save');
-var deleteBtn = $('.delete');
+// Getting a handle on the buttons
+var saveBtn9 = $('#button9');
+var saveBtn10 = $('#button10');
+var saveBtn11 = $('#button11');
+var saveBtn12 = $('#button12');
+var saveBtn1 = $('#button1');
+var saveBtn2 = $('#button2');
+var saveBtn3 = $('#button3');
+var saveBtn4 = $('#button4');
+var saveBtn5 = $('#button5');
 
-saveBtn.click(function(){
-    console.log(form9)
-});
+
+
+
 
 
 
